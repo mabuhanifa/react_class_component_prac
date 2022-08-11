6f310facd45dec9ef7d008797302f9444b8f297c
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-11 16:13:16 +0600

    counter updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div>
-    gggggggggggggggggggggggggggg
+    <Counter/>
     </div>
   )
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class Counter extends Component {
-    constructor() {
-        super();
-        this.state = {color: "red"};
-      }
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 5,
+    };
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
   render() {
     return (
-      <div>Counter</div>
-    )
+      <div className="counter">
+        <h4>Count: {this.state.count}</h4>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
   }
 }
+
+export default Counter;
