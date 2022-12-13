1b70eebbadd5b9646053cbed75d0f0addeabebec
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-13 09:19:48 +0600

    createState

--- src/components/Custom.jsx
+++ src/components/Custom.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+
+let state;
+
+function createState(setState) {
+  if (state === undefined) {
+    state = setState;
+  }
+  function getState(newState) {
+    state = newState;
+  }
+  return [state, getState];
+}
 
 export default function Custom() {
-  return (
-    <div>Custom</div>
-  )
+  return <div>Custom</div>;
 }
